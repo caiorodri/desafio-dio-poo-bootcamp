c38768f37d60d5b0c801271c390e127a5d9558cf
caiorodri <caiodossantosrodrigues@gmail.com>
2023-03-16 16:02:17 -0300

    Created

---
+++ src/com/caio/Conteudo.java
@@ -0,0 +1,29 @@
+package com.caio;
+
+public class Conteudo {
+
+	private String titulo;
+	private String descricao;
+	protected final double XP_PADRAO = 10d;
+	
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
+}
